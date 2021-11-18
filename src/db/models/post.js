f39b5df10f69bef5d834528afe62ccdd0e3359da
Nico <tenshito27@gmail.com>
2021-11-18 16:51:18 +0300

    db the end

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -3,25 +3,23 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Pair extends Model {
+  class Post extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({User}) {
-      this.belongsToMany(User, {
-        through:"Schedules",
-        foreignKey:"pair_id"
-      })
+    static associate(models) {
+      // define association here
     }
   };
-  Pair.init({
-    user_id: DataTypes.INTEGER,
-    sport_id: DataTypes.INTEGER
+  Post.init({
+    title: DataTypes.STRING,
+    body: DataTypes.TEXT,
+    img: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Pair',
+    modelName: 'Post',
   });
-  return Pair;
+  return Post;
 };
